31f8583200a0e94759b20099db64f892991f9ab6
qooba <dev@qooba.net>
2022-07-25 23:12:14 +0200

    change run yummy

--- run.yummy.sh
+++ run.yummy.sh
@@ -3,4 +3,4 @@
 docker run -it -p 8887:8888 --rm --shm-size=5.09gb --name yummy \
 	--network app_default \
 	-v $(pwd)/notebooks:/home/jovyan/notebooks \
-	qooba/yummy:v0.0.2_spark /bin/bash
+	qooba/yummy:v0.0.2_spark /home/jovyan/notebooks/jupyter.sh
